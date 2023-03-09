eec3d39af7cb4b69b16f62c1f151ed96b860f6de
iucario <16816842+iucario@users.noreply.github.com>
2023-03-09 19:30:29 +0900

    feat: download util

---
+++ src/utils/download.js
@@ -0,0 +1,20 @@
+const searchNextjsData = (html) => {
+  const nextjsData = html.matchAll(
+    /<script[^>]+id=[\'"]__NEXT_DATA__[\'"][^>]*>([^<]+)<\/script>/g
+  )
+  for (const match of nextjsData) {
+    const data = JSON.parse(match[1])
+    return data
+  }
+}
+
+const testUrl =
+  'https://www.ted.com/talks/julian_treasure_5_ways_to_listen_better/transcript?language=en'
+
+export const parseParagraphs = async (url) => {
+  const resp = await fetch(url)
+  const data = searchNextjsData(await resp.text())
+  const transcriptData = data.props.pageProps.transcriptData
+  const paragraphs = transcriptData.translation.paragraphs
+  return paragraphs
+}
