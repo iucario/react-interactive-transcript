d8a5ad59c7a1e639b11c19f3ea59f23401679829
iucario <16816842+iucario@users.noreply.github.com>
2023-03-10 11:05:17 +0900

    feat: show file name after upload

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 function Upload({ onUpload }) {
   const [audioFile, setAudioFile] = useState(null)
   const [transcriptFile, setTranscriptFile] = useState(null)
+  const audioFileRef = useRef(null)
+  const transcriptFileRef = useRef(null)
 
   const handleAudioChange = (event) => {
     const file = event.target.files[0]
     setAudioFile(file)
+    audioFileRef.current.innerText = file.name
   }
 
   const handleTranscriptChange = (event) => {
     const file = event.target.files[0]
     setTranscriptFile(file)
+    transcriptFileRef.current.innerText = file.name
   }
 
   const handleUpload = () => {
@@ -21,26 +25,26 @@ function Upload({ onUpload }) {
   return (
     <div className="upload-container">
       <div className="upload-audio">
-        <label htmlFor="audio-file">
-          Upload audio file
-          <input
-            type="file"
-            id="audio-file"
-            accept="audio/*"
-            onChange={handleAudioChange}
-          />
+        <label htmlFor="audio-file" ref={audioFileRef}>
+          Upload audio file. (*.mp3, *.wav, ...)
         </label>
+        <input
+          type="file"
+          id="audio-file"
+          accept="audio/*"
+          onChange={handleAudioChange}
+        />
       </div>
       <div className="upload-transcript">
-        <label htmlFor="transcript-file">
-          Upload transcript file
-          <input
-            type="file"
-            id="transcript-file"
-            accept=".json"
-            onChange={handleTranscriptChange}
-          />
+        <label htmlFor="transcript-file" ref={transcriptFileRef}>
+          Upload transcript file. (*.json)
         </label>
+        <input
+          type="file"
+          id="transcript-file"
+          accept=".json"
+          onChange={handleTranscriptChange}
+        />
       </div>
       <button onClick={handleUpload} className="btn">
         Upload
